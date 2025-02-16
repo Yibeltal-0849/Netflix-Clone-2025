775c90709b76c5393b3a07ab81994e65f8ec2709
Yibeltal-0849 <yibeltal2514@gmail.com>
2025-02-16 23:08:39 +0300

    banner part or background image

--- Netflix-Website/src/Pages/Home/Home.jsx
+++ Netflix-Website/src/Pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
+      {/* <h1>hi</h1>
+      <h1>hi</h1> */}
       <Footer />
     </>
   );
